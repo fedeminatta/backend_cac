15196458982b80eb1c744aea6760f366fb9061f8
fedeminatta <minattafederico@gmail.com>
2024-07-04 19:09:03 -0300

    database user

--- db/config/mysql.config.js
+++ db/config/mysql.config.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 const config = {
 	host: process.env.DATABASE_HOST,
 	port: process.env.DATABASE_PORT,
-	user: 'root',
+	user: process.env.DATABASE_USER || 'root',
 	password: '' || process.env.DATABASE_PASSWORD,
 	database: process.env.DATABASE_NAME,
 };
